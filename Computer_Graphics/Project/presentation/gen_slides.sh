78d1abae30bb5d6c0965a7e8123a5618e3b2a5ba
miaodx <miaodx@hotmail.com>
2017-06-15 13:54:37 +0800

    update presentation

---
+++ Computer_Graphics/Project/presentation/gen_slides.sh
@@ -0,0 +1,2 @@
+pandoc -f markdown -t revealjs --standalone --self-contained presentation.md -o presentation.html -V theme=serif2 -V revealjs-url=H:/class_material/hand_in_homework/pandoc_markdown_revealjs/reveal.js -i
+#pandoc -f markdown -t revealjs --standalone --self-contained presentation.md -o presentation.html -V theme=serif2 -V revealjs-url=/home/miao/github/hand_in_homework/pandoc_markdown_revealjs/reveal.js -i
